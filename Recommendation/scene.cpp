d6a85bb29742c26526b0d83181885216c08deaf1
njuzss <zss.ai.3@gmail.com>
2016-05-10 21:44:12 +0800

    add scene

---
+++ Recommendation/scene.cpp
@@ -0,0 +1,57 @@
+#include "scene.h"
+
+int Scene::determin(int index)
+{
+	int num = 0;
+	while ((0 < index) && (index > View::maxK))
+	{
+		index = index - View::maxK;
+		num++;
+	}
+	return num%View::nview;
+}
+
+bool Scene::filterMatch(pair<int, int> match)
+{
+	int l = determin(match.first);
+	int r = determin(match.second);
+	return l != r;
+}
+
+void Scene::getCrossView(string path)
+{
+	ifstream ifs;
+	ifs.open(path);
+	if (ifs.fail())
+	{
+		cout << "failed to open " << path << endl;
+	}
+
+	while (!ifs.eof())
+	{
+		string left, right;
+		int x = 0, y = 0;
+		string::size_type p, q;
+		getline(ifs, left);
+
+		p = left.find_first_of('=');
+		cout << p << endl;
+		x = atoi(left.substr(5, p - 5).c_str());
+		cout << x << endl;;
+		right = left.substr(p + 3);
+
+		q = right.find_first_of('=');
+		cout << p << endl;
+		y = atoi(right.substr(5, q - 5).c_str());
+		cout << y << endl;;
+
+		pair<int, int> match(x, y);
+		cout << match.first << match.second << endl;
+		if (filterMatch(match))
+		{
+			this->cross.push_back(match);
+		}
+	}
+
+	ifs.close();
+}
